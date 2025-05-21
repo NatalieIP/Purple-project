7d1d8b181237c79c6dbd3674e4b889d943783257
Natalie <natalieajani@gmail.com>
2025-05-21 16:31:45 +0100

    downloaded 2 applications and rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import './App.css'
 
 function App() {
   
+let name = "Raven";
+let score = 40 + 39;
 
   return (
     <>
-      <div>
-       This is a new Vite project
-      </div>
+   <div>
+    This is a new Vite project for {name}
+   </div>
+<div className="wrapped">
+<p className="">My score is {score}</p>
+<h2 className="">This is a h2 tag</h2>
+</div>
+    </div>
+      
       
     </>
   )
